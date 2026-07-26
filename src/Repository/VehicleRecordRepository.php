7c086b71edfaff7ce431a7eff031b873c5b6b214
Laurynas M <laurynas.mazuolis@bartusit.com>
2026-07-26 19:14:43 +0300

    stan quirk

--- src/Repository/VehicleRecordRepository.php
+++ src/Repository/VehicleRecordRepository.php
@@ -7,7 +7,7 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends VehicleRecordRepository<VehicleRecord>
+ * @extends ServiceEntityRepository<VehicleRecord>
  */
 class VehicleRecordRepository extends ServiceEntityRepository
 {
